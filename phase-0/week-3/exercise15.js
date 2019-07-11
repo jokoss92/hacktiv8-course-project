abe7f97acda4ffaaadfd1ccb10f393a4488310f7
Joko Sarjono Slameto <johno.smakaduta@gmail.com>
2019-07-11 09:59:20 +0700

    Add Exercise 15

--- phase-0/week-3/exercise15.js
+++ phase-0/week-3/exercise15.js
@@ -1,5 +1,24 @@
 function groupAnimals(animals) {
     // you can only write your code here!
+    animals = animals.sort()
+    var tempArray = []
+    var resultArray = []
+    var init = animals[0][0]
+
+    for (var i=0; i<animals.length; i++){
+        if(init === animals[i][0]){
+            tempArray.push(animals[i]);
+            if(i === (animals.length-1)){
+                resultArray.push(tempArray)
+            } 
+        } else {
+            resultArray.push(tempArray)
+            tempArray = []
+            tempArray.push(animals[i])
+            init = animals[i][0]
+        }
+    }
+    return resultArray
   }
   
   // TEST CASES
